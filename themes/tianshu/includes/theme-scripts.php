fa816e690ccc522174a1be6d18cde184edfe397f
diep.lk <diep.lk@kiotviet.com>
2025-10-22 18:00:16 +0700

    update core

--- themes/tianshu/includes/theme-scripts.php
+++ themes/tianshu/includes/theme-scripts.php
@@ -65,7 +65,7 @@ function tianshu_front_end_scripts(): void
     // load google fonts
     wp_enqueue_style(
         'google-fonts',
-        'https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Roboto+Condensed:wght@400;500;700&display=swap',
+        'https://fonts.googleapis.com/css2?family=Crimson+Pro:wght@400;500;600&family=Lora:wght@500;600&display=swap',
         [],
         null
     );
