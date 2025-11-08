a5d669d7808123eb12dc321dd5f1ef6c42580726
lathieuhiep <khacdiepkma90@gmail.com>
2025-11-08 10:39:07 +0700

    update plugin

--- themes/tianshu/core/tgm/plugin-activation.php
+++ themes/tianshu/core/tgm/plugin-activation.php
@@ -22,40 +22,11 @@ function tianshu_register_required_plugins(): void {
 		),
 
 		// This is an example of how to include a plugin from the WordPress Plugin Repository
-        array(
-            'name'      =>  'CMB2',
-            'slug'      =>  'cmb2',
-            'required'  =>  true,
-        ),
-
-		// This is an example of how to include a plugin from the WordPress Plugin Repository
 		array(
 			'name'      =>  'Elementor',
 			'slug'      =>  'elementor',
 			'required'  =>  true,
 		),
-
-		// This is an example of how to include a plugin from the WordPress Plugin Repository
-		array(
-			'name'      =>  'Breadcrumb Navxt',
-			'slug'      =>  'breadcrumb-navxt',
-			'required'  =>  false,
-		),
-
-		// This is an example of how to include a plugin from the WordPress Plugin Repository
-		array(
-			'name'      =>  'Contact Form 7',
-			'slug'      =>  'contact-form-7',
-			'required'  =>  true,
-		),
-
-        // This is an example of how to include a plugin from the WordPress Plugin Repository
-		array(
-			'name'      =>  'Loco Translate',
-			'slug'      =>  'loco-translate',
-			'required'  =>  false,
-		),
-
 	);
 
 	/**
